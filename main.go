986b21d01ae2d2990c7190d8d21111d0751f01e2
eastrocky <57242461+eastrocky@users.noreply.github.com>
2020-02-11 19:19:39 -0600

    control go routines with waitgroups

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	go count("sheep")
-	count("fish")
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// WaitGroup is none of count's concern.
+	// Use anonymous function to control group
+	go func() {
+		count("sheep")
+		wg.Done()
+	}()
+
+	// wait for it...
+	wg.Wait()
 }
 func count(thing string) {
-	for i := 0; true; i++ {
+	for i := 0; i < 5; i++ {
 		fmt.Println(i, thing)
 		time.Sleep(time.Millisecond * 500)
 	}
