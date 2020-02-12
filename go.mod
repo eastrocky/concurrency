9ce0301e744684b5b433e873e7e8da9d2b049706
eastrocky <57242461+eastrocky@users.noreply.github.com>
2020-02-11 19:12:09 -0600

    count sheep

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/eastrocky/concurrency
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	count("sheep")
+	count("fish")
+}
+func count(thing string) {
+	for i := 0; true; i++ {
+		fmt.Println(i, thing)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
